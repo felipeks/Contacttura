eab382387fc57036698d713142c387ab6609038f
felipe Seabra <felipeks25@gmail.com>
2020-12-12 12:37:24 -0300

    feat (Contacttura): criação do projeto contacttura delete, listar, cadastrar e atualiza

---
+++ contacttura/contacttura/src/main/java/com/contacttura/contacttura/ContactturaApplication.java
@@ -0,0 +1,36 @@
+package com.contacttura.contacttura;
+
+import java.util.stream.LongStream;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.contacttura.contacttura.model.Contacttura;
+import com.contacttura.contacttura.repository.ContactturaRepository;
+
+@SpringBootApplication
+public class ContactturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactturaApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner init (ContactturaRepository repository) {
+		return args -> {
+		//opcional
+			repository.deleteAll();
+			LongStream.range(1, 11)
+			.mapToObj( i -> {
+				Contacttura c = new Contacttura();
+				c.setName("Contacttura User" + i);
+				c.setEmail("contacttura" + i + "@gmail.com");
+				c.setPhone("(081) 9"+ i +i +i +i +"-"+ i +i +i +i);
+				return c;
+			}).map(m -> repository.save(m))
+				.forEach(System.out::println);
+		};
+	}
+}
